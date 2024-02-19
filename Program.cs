5eb024ca47b8a2eeed8002dc4f727b380eebc81a
Garik <gar.minasyan.97@mail.ru>
2024-02-19 09:40:33 +0400

    Создали цикл for,что бы пользователь мог вводить строки для массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// Создаем массив строк `str` случайной длины от 1 до 10.
 string[] str = new string[new Random().Next(1, 11)];
+
+// Запрашиваем ввод от пользователя  строк для каждого элемента массива `str`.
+for (int i = 0; i < str.Length; i++)
+{
+    Console.Write($"Введите {i} элемент для массива: ");
+    str[i] = Console.ReadLine()!;
+}
